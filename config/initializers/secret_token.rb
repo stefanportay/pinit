52db69f35a56723592d5c8318b5bd482a990b21c
Stefan Portay <stefanportay@gmail.com>
2013-07-20 10:05:52 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinit::Application.config.secret_token = 'a5c2f0df86182c5f589af8a2859a9c295ee4ea6f3f9a8d7aab7ade646c489bf79ae5acdbca0905e1b4a1343d74f77c8b054def17954e171cb4720824c21951f7'
